460a00f62ff3bad9ba4035aa42620da496bab8f2
SachinSarin <60125906+SachinSarin@users.noreply.github.com>
2021-08-20 23:52:01 +0530

    Basic Problem revise only once

---
+++ Easy/Problem_9_ Length of Last Word.cpp
@@ -0,0 +1,54 @@
+/*
+Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+*/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+     
+      int count  = 0;
+      if(s.size()==1)
+      {
+          return 1;
+      }
+      for(int i = s.size()-1 ; i >=0; i--)
+      {
+          if(s[i]!=' ')
+          {
+              count++;
+          }
+          if(s[i]==' '&&count > 0)
+          {
+              return count;
+          }
+      }
+        return count;
+    }
+};
